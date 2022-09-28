eb3e62075139034fd21ae09d022255f41aaa8e10
Riley Fletcher <89424122+nrfletcher@users.noreply.github.com>
2022-09-27 20:02:36 -0400

    Create trivia_test.py

---
+++ tests/trivia_test.py
@@ -0,0 +1,43 @@
+import unittest
+
+
+class Question:
+    def __init__(self, string):
+        self.string = string
+
+
+def cleanup(string):
+    new_string = string
+    if '&#039;' in string:
+        new_string = new_string.replace('&#039;', '\'')
+    if '&quot;' in string:
+        new_string = new_string.replace('&quot;', '"')
+    return new_string
+
+
+class TestQuestion(unittest.TestCase):
+
+    def test_quote(self):
+        question = Question('The movie &quot;The Lord of the Rings&quot; is one of the best ever made.')
+        question = cleanup(question)
+        expected = 'The movie "The Lord of the Rings" is one of the best ever made.'
+        error = 'These do not match'
+        self.assertEqual(question, expected, error)
+
+    def test_apostrophe(self):
+        question = Question('My name&#039;s Brad, how &#039;bout you?')
+        question = cleanup(question)
+        expected = 'My name\'s Brad, how \'bout you?'
+        error = 'These do not match'
+        self.assertEqual(question, expected, error)
+
+    def test_both_params(self):
+        question = Question(
+            'Which of these languages was NOT included in the 2016 song &quot;Don&#039;t Mind&quot; by Kent Jones?')
+        question = cleanup(question)
+        expected = 'My name\'s Brad, how \'bout you?'
+        error = 'These do not match'
+        self.assertEqual(question, expected, error)
+
+
+unittest.main()

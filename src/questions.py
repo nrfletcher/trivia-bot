69b4eb951f6a8e90cf31f9f87a88686741d741de
Riley Fletcher <89424122+nrfletcher@users.noreply.github.com>
2022-12-07 19:20:43 -0500

    Refactored comments

--- src/questions.py
+++ src/questions.py
@@ -1,4 +1,5 @@
-# Convert JSON-encoded text (or doubly encoded) back to their proper text
+''' Helper function for converting JSON into proper text format
+    When retrieving from API special characters don't appear to translate properly '''
 
 def cleanup(string):
     new_string = string
@@ -14,6 +15,7 @@ def cleanup_test(string='&quot; empty space &#039;'):
     print('The movie &quot;The Lord of the Rings&quot; is one of the best ever made.')
 
 
-print(cleanup('The movie &quot;The Lord of the Rings&quot; is one of the best ever made.'))
-print(cleanup('My name&#039;s Brad, how &#039;bout you?'))
-print(cleanup('Which of these languages was NOT included in the 2016 song &quot;Don&#039;t Mind&quot; by Kent Jones?'))
+def tests():
+    print(cleanup('The movie &quot;The Lord of the Rings&quot; is one of the best ever made.'))
+    print(cleanup('My name&#039;s Brad, how &#039;bout you?'))
+    print(cleanup('Which of these languages was NOT included in the 2016 song &quot;Don&#039;t Mind&quot; by Kent Jones?'))
